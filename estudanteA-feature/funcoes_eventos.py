dcd4fb64800f321ffbdd8fd922997af5eb158393
FelipeMadson <felipemadson@gmail.com>
2025-09-25 17:46:42 -0300

    Adiciona nova feature, juntamente com nova função construtora de evento

---
+++ estudanteA-feature/funcoes_eventos.py
@@ -0,0 +1,10 @@
+def evento(id, nome, data, local, categoria, participado):
+    return {
+        "id": id,
+        "nome": nome,
+        "data": data,
+        "local": local,
+        "categoria": categoria,
+        "participado": participado,
+    }
+#função construtora de evento
